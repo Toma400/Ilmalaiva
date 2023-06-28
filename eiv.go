c809b4f75140d1d1e9f5e352375ce523b246f4b4
Toma400 <thomasso_calgori@o2.pl>
2023-06-28 15:09:16 +0200

    First flight in the sky

---
+++ eiv.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+)
+
+func init() {
+
+}
+
+type Game struct {
+
+}
+
+func (g *Game) Update() error {
+
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+
+}
+
+func main() {
+	
+}
